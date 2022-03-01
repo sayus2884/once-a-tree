80c4fc997db349a56a82538db0a13bf5c920522c
sayus2884 <joniah2884@gmail.com>
2022-02-28 20:21:01 -0800

    add Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface Props
+  extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  className?: string;
+  label?: string;
+}
+
+const Input: React.FC<Props> = ({ className, label, ...props }) => {
+  return (
+    <label className="flex flex-col gap-1">
+      {label}
+      <input className={`${className} ring-none p-2`} {...props} />
+    </label>
+  );
+};
+
+export default Input;
