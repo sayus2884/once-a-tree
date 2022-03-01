4c19ccda1b179e98abf9fabf063698ac7e84aaa7
sayus2884 <joniah2884@gmail.com>
2022-02-28 23:56:10 -0800

    style: add rounded corner

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,7 +8,7 @@ interface Props
 
 const Input: React.FC<Props> = ({ className, label, ...props }) => {
   return (
-    <label className="flex flex-col gap-3">
+    <label className="flex flex-col gap-3 rounded border border-black/20">
       {label}
       <input className={`${className} ring-none p-2`} {...props} />
     </label>
