c4abf2d071a9a2722d4dfac3a8735b802e02c833
sayus2884 <joniah2884@gmail.com>
2022-03-01 17:23:49 -0800

    fix: add black text

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -10,7 +10,10 @@ const Input: React.FC<Props> = ({ className, label, ...props }) => {
   return (
     <label className="flex flex-col gap-3 rounded font-light tracking-wide">
       {label}
-      <input className={`${className} ring-none p-2 border border-black/20`} {...props} />
+      <input
+        className={`${className} ring-none p-2 border border-black/20 text-black`}
+        {...props}
+      />
     </label>
   );
 };
