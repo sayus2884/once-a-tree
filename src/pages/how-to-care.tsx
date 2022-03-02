f2cc232f72f7db7272d1ffe805f67431c02fd118
sayus2884 <joniah2884@gmail.com>
2022-03-02 01:56:48 -0800

    add how to care page

---
+++ src/pages/how-to-care.tsx
@@ -0,0 +1,62 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import React from "react";
+import { careInstructions } from "../utils/constants";
+
+const HowToCare: NextPage = () => {
+  return (
+    <section className="page-px page-py">
+      <div className="grid gap-8 max-w-[1000px]">
+        <h2 className="title">Make Your Furniture Last Longer</h2>
+        <p>
+          At Once a Tree, we create unique spaces and understand that your home&apos;s furniture and
+          decor are part of your lifestyle and, therefore, should be cared for in a way that expands
+          their natural longevity. Whether you&apos;re having a high-end furniture item custom made,
+          or buying something from one of our trusted Canadian brands, knowing how to care for each
+          piece is the key to an elegant living space.
+        </p>
+        <p>
+          Continue reading on how to take care for the different kinds of materials you will find in
+          our catalogue:{" "}
+          <a className="underline underline-offset-1 hover:opacity-80" href="#wood">
+            Wood
+          </a>
+          ,{" "}
+          <a className="underline underline-offset-1 hover:opacity-80" href="#upholstery">
+            Upholstery
+          </a>
+          ,{" "}
+          <a className="underline underline-offset-1 hover:opacity-80" href="#fabric">
+            Fabric
+          </a>
+          , and{" "}
+          <a className="underline underline-offset-1 hover:opacity-80" href="#leather">
+            Leather
+          </a>
+          .
+        </p>
+
+        {careInstructions.map(({ type, todos }) => (
+          <div id={type}>
+            <h3 className="text-2xl font-bold mb-3">{type}</h3>
+            <ul className="list-disc ml-5">
+              {todos.map((todo) => (
+                <li>{todo}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+
+        <p>
+          Do you have any questions about the caring of a luxury furniture or décor item?{" "}
+          <Link href={"/contact"}>
+            <a className="underline underline-offset-1 hover:opacity-80">Contact us</a>
+          </Link>{" "}
+          and we will be happy to help.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HowToCare;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -69,3 +69,47 @@ export const categories = [
   { name: "Rugs", route: "/" },
   { name: "Outdoor", route: "/" },
 ];
+
+export const careInstructions = [
+  {
+    type: "wood",
+    todos: [
+      "Dust with dry, soft cloth or untreated dusting cloth.",
+      "Avoid alcohol or petroleum-based products on lacquer finishes.",
+      "Felt pads should be used on items placed on wood surfaces to avoid scratches.",
+      "Irregularities such as sapwood, knots and uneven colouration should be expected in natural wood and enhance the inherent beauty of the material.",
+      "Over time, air and sunlight will change the colour of natural wood. You can occasionally rotate items placed on your wood furniture to allow a more uniform colour to develop.",
+      "To ensure even colouration of extension table leaves, expose them to the same light as your table. Once the leaves have reached mature coloration, they may be stored flat and top down.",
+    ],
+  },
+  {
+    type: "upholstery",
+    todos: [
+      "While all fabric will fade over time, direct sunlight can dramatically fade dark colours. We recommend placing your upholstered pieces where they won’t be exposed to direct sunlight.",
+      "An annual professional cleaning will keep your sofa or chair looking its best.",
+      "Cushions will compress over time. Comfort wrinkles and creases occur naturally as upholstered furniture adjusts to normal use. Fluff and rotate cushions weekly to keep them looking fresh and to help them wear evenly.",
+      "Vacuum weekly with an upholstery attachment. Although you may not see it, dust settles on your upholstery and can soil or cause wearing.",
+    ],
+  },
+  {
+    type: "fabric",
+    todos: [
+      "Prompt attention to your stain is advisable. Clean up as much of the spill as possible with a clean, dry, white cloth. Blot - don’t rub.",
+      "Please call a Design Associate for the fabric cleaning code of your piece before using anything.",
+      "If the stain has dried, remove excess by gently using a soft nylon brush.",
+      "Never use a cleaning solvent containing bleaching agents. Test a mild upholstery cleaning solution in an inconspicuous area. Begin with a few drops of the solution on a cloth, hold it against the colour and wait a few seconds. Look to see if any of the other colour has transferred to the cloth or if there has been a change in the colour of the fabric. If so, contact an upholstery cleaning specialist.",
+      "Apply a small amount of a mild cleaning solution to the cloth. Over saturation may affect the upholstery materials (backing, foam, etc.) under the fabric.",
+    ],
+  },
+  {
+    type: "leather",
+    todos: [
+      "To prevent fading and cracking, avoid exposure to direct sunlight or heat.",
+      "Fluff seat and back cushions to help them wear more evenly.",
+      "Dust and vacuum regularly.",
+      "For spills, we recommend using distilled water and a light coloured cloth.",
+      "Do not use water to wash out butter, oil or grease stains. Wipe dry with cloth. The spot will dissipate into the leather over time.",
+      "To protect and maintain leather we recommend purchasing a leather conditioner.",
+    ],
+  },
+];
