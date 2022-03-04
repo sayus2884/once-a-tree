723ca23281f85cbdba7754c346c812fb9d402b5d
sayus2884 <joniah2884@gmail.com>
2022-03-04 00:34:56 -0800

    navigate to item page on card click

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Heart } from "phosphor-react";
+import { useRouter } from "next/router";
 
 interface Props {
   className?: string;
@@ -8,17 +9,30 @@ interface Props {
   description: string;
   price: string;
   isLiked?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-const Card: React.FC<Props> = ({ className, ...props }) => {
+const Card: React.FC<Props> = ({ className, onClick, ...props }) => {
   const { name, description, price, isLiked } = props;
+  const router = useRouter();
+
+  const handleOnCardClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    if (onClick) onClick(event);
+
+    // Move to item page
+    router.push(`/item/${name}`);
+  };
+
   return (
-    <div className={`${className} border border-black/20 rounded bg-white shadow`} {...props}>
-      <div className="relative h-[350px] w-full ">
+    <div
+      className={`${className} border border-black/20 rounded bg-white shadow`}
+      onClick={handleOnCardClick}
+      {...props}>
+      <div className={`relative h-[350px] w-full cursor-pointer`}>
         <Image src="/images/chair_sample-black.jpg" layout="fill" objectFit="contain" />
       </div>
       <div className="grid gap-5 p-5">
-        <h2 className="font-bold text-2xl">{name}</h2>
+        <h2 className={`font-bold text-2xl cursor-pointer`}>{name}</h2>
         <p>{description}</p>
         <div className="flex justify-between items-center">
           <p className="font-light text-xl">$ {price}</p>
